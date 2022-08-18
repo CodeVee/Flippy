079fa06c7bd9831d884c73f09195cb34494774ff
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-08-18 17:19:45 +0100

    updated bass resources

--- Flippy/MauiProgram.cs
+++ Flippy/MauiProgram.cs
@@ -9,9 +9,9 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("Lato-Regular.ttf", "LatoRegular");
+                fonts.AddFont("Lato-Bold.ttf", "LatoBold");
+            });
 
 		return builder.Build();
 	}
