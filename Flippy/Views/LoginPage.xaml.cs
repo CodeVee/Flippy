db628a2840cd5cd7adb8d78aef08468318c2831f
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-08-19 22:14:11 +0100

    added register page

--- Flippy/Views/LoginPage.xaml.cs
+++ Flippy/Views/LoginPage.xaml.cs
@@ -11,4 +11,9 @@ public partial class LoginPage : ContentPage
 	{
 		passwordEntry.IsPassword = !passwordEntry.IsPassword;
 	}
+
+    private async void RegisterButton_Clicked(object sender, EventArgs e)
+    {
+		await Shell.Current.GoToAsync($"{nameof(RegisterPage)}");
+    }
 }
---
+++ Flippy/Views/RegisterPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Flippy.Views;
+
+public partial class RegisterPage : ContentPage
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void LoginButton_Clicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync($"{nameof(LoginPage)}");
+	}
+}
