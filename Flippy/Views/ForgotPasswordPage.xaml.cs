a8860eec90a2a0d639e37de4eb57ea4b7763e5f2
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-08-19 22:31:03 +0100

    added reset password page

--- Flippy/Views/ForgotPasswordPage.xaml.cs
+++ Flippy/Views/ForgotPasswordPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class ForgotPasswordPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void SubmitButton_Clicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync(nameof(ResetPasswordPage));
+	}
 }
