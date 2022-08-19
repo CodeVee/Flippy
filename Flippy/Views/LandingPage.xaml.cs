38e21a64b49605262b35f5d09c2d41b53779a44d
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-08-19 13:48:28 +0100

    Add Landing page

--- Flippy/Views/LandingPage.xaml.cs
+++ Flippy/Views/LandingPage.xaml.cs
@@ -6,4 +6,31 @@ public partial class LandingPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void OnPositionChanged(object sender, PositionChangedEventArgs e)
+	{
+		var position = carousel.Position;
+
+		if (position < 2)
+			return;
+
+		carousel.IsSwipeEnabled = false;
+        footer.IsVisible = false;
+		startBtn.IsVisible = true;
+	}
+
+	private void OnSkipButton_Clicked(object sender, EventArgs e)
+	{
+		carousel.ScrollTo(2);
+	}
+
+	private void OnNextButton_Clicked(object sender, EventArgs e)
+	{
+		var position = carousel.Position;
+		if (position > 1)
+			return;
+
+		var nextPosition = position + 1;
+		carousel.ScrollTo(nextPosition);
+	}
 }
