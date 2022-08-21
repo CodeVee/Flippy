f51076fdb9feae07fdbce009581e3b52b2977a95
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-08-21 21:55:03 +0100

    added product list page

--- Flippy/Views/HomePage.xaml.cs
+++ Flippy/Views/HomePage.xaml.cs
@@ -21,4 +21,29 @@ public partial class HomePage : ContentPage
             new() { Name = "Mushrooms", Price = 150, Weight = "1KG" , ImageSource = "mushroom.png" },
         };
 	}
+
+	private async void AllButton_Clicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync($"//{nameof(CategoryPage)}");
+	}
+
+    private async void BestSellerButton_Clicked(object sender, EventArgs e)
+    {
+        var pageTitle = "Best Sellers";
+        var navigationParameter = new Dictionary<string, object>
+        {
+            { "PageTitle", pageTitle }
+        };
+        await Shell.Current.GoToAsync(nameof(ProductListPage), navigationParameter);
+    }
+
+    private async void FruitButton_Clicked(object sender, EventArgs e)
+    {
+        var pageTitle = "Fruits";
+        var navigationParameter = new Dictionary<string, object>
+        {
+            { "PageTitle", pageTitle }
+        };
+        await Shell.Current.GoToAsync(nameof(ProductListPage), navigationParameter);
+    }
 }
