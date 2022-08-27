a55484596aad2b9adbe98414cd7dcc36d280d928
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-08-27 07:49:29 +0100

    added search page

--- Flippy/Views/SearchPage.xaml.cs
+++ Flippy/Views/SearchPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class SearchPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		var text = e.NewTextValue.Trim();
+		var textlength = text.Length;
+		if (textlength == 0)
+			clearBtn.IsVisible = false;
+		else
+			clearBtn.IsVisible = true;
+	}
+
+	private void ClearBtn_Clicked(object sender, EventArgs e)
+	{
+		searchEntry.Text = "";
+	}
 }
